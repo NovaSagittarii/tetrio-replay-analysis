40cd26c66f2396f1507fca8368c1bfae489b17bd
NovaSagittarii <novasagittarii@gmail.com>
2023-07-29 15:47:41 -0700

    read json

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
+import json
 
 uploaded_files = st.file_uploader("Choose a TTRM file", accept_multiple_files=True)
 for uploaded_file in uploaded_files:
-  bytes_data = uploaded_file.read()
-  st.write("filename:", uploaded_file.name)
-  st.write(bytes_data)
+  obj = json.load(uploaded_file)
+  st.write(obj)
